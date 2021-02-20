4dc9c1207c0b1748f0503c65cbdf2eb7ad85b8c8
Thomas Miller <tmiv74@Thomass-MacBook-Pro.local>
2021-02-19 22:06:34 -0800

    execute sfu

--- start-sfu.sh
+++ start-sfu.sh
@@ -1,4 +1,12 @@
 #! /usr/bin/bash
 
-export EXTERNAL_IP=$(curl -H "Metadata-Flavor: Google" http://metadata/computeMetadata/v1/instance/network-interfaces/0/access-configs/0/external-ip)
+export EXTERNAL_IP=$(curl -s -H "Metadata-Flavor: Google" http://metadata/computeMetadata/v1/instance/network-interfaces/0/access-configs/0/external-ip)
+
+if [ -z $EXTERNAL_IP ] 
+    then
+       export EXTERNAL_IP=127.0.0.1
+fi
+
+cat /srv/config.template.toml | sed "s/%%IP_ADDRESS_EXT%%/$EXTERNAL_IP/g" > /srv/config.toml
+/srv/sfu -c /srv/config.toml
 
