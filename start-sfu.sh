5708d663a19d0fd7402f89494c0a97035a384064
Thomas Miller <tmiv74@Thomass-MacBook-Pro.local>
2021-02-19 21:36:08 -0800

    curl the ip

--- start-sfu.sh
+++ start-sfu.sh
@@ -1,3 +1,4 @@
-#! /usr/local/bin/bash
+#! /usr/bin/bash
 
+export EXTERNAL_IP=$(curl -H "Metadata-Flavor: Google" http://metadata/computeMetadata/v1/instance/network-interfaces/0/access-configs/0/external-ip)
 
